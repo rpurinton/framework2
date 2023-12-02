1093a77bed6c8d2492f27ee018f212822f43ff18
Russell Purinton <russell.purinton@gmail.com>
2023-12-02 10:46:48 -0500

    Update Error.php

--- src/Error.php
+++ src/Error.php
@@ -7,7 +7,6 @@ class Error extends \Exception implements \Throwable
     public function __construct(protected $message, protected $code = 0, protected ?\Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
-        echo ("Custom Error Handler: " . $this->__toString());
     }
 
     public function __toString(): string
